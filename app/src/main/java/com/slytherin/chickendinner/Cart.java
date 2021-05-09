16c9a6301966c6273802acbe4914ed7805bc1e62
madhushaniw96 <acer@LAPTOP-F4V1HCBK>
2021-05-09 12:24:30 +0530

    changes

--- app/src/main/java/com/slytherin/chickendinner/Cart.java
+++ app/src/main/java/com/slytherin/chickendinner/Cart.java
@@ -20,8 +20,8 @@ public class Cart extends AppCompatActivity {
         btn_Cart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent cart = new Intent(Cart.this, AddToCart.class);
-                startActivity(cart);
+                Intent cart_View = new Intent(Cart.this, AddToCart.class);
+                startActivity(cart_View);
             }
         });
     }
